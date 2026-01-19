e3859bc2b272cfbc363a08c489abfaad4dda28f8
lahiruharshana <lharshana2002@gmail.com>
2026-01-19 12:08:03 +0530

    Fix: Move MONGODB_URI check to runtime

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
@@ -28,6 +24,12 @@ if (!global.mongoose) {
 }
 
 async function dbConnect(): Promise<typeof mongoose> {
+  const MONGODB_URI = process.env.MONGODB_URI!;
+
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
